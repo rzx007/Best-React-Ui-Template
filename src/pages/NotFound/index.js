fb03b4e93006ffbe62b6df9bfdad49e12e1ad71b
阮志雄 <770304867@qq.com>
2020-08-11 14:32:27 +0800

    404

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
-class NotFound extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-    render() {
-        console.log(this.props);
-        return (
-            <div>
-                This is 404 Page url{this.props.location.pathname}
-            </div>
-        );
-    }
-}
+import React, { useState, useEffect } from 'react';
+import { Button } from "antd";
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import "./index.less";
 
+function NotFound(props) {
+    let [count, setCount] = useState(5);
+    useEffect(() => {
+        let timer = setInterval(() => {
+            if (!count) {
+                goBack()
+                clearInterval(timer)
+            }
+            setCount(count--)
+        }, 1000)
+        return () => {
+            clearInterval(timer)
+        }
+    })
+    function goBack() {
+        props.history.go(-1)
+    }
+    return (
+        <div className="error-page">
+            <div className="error-container">
+                <div className="errorContainer">
+                    <h1 className="errorCode text-primary">404</h1>
+                    <p className="errorInfo">
+                        哦豁, it seems that {props.location.pathname} does not exist.
+                    </p>
+                    <Button type="primary" onClick={() => goBack()} icon={<ArrowLeftOutlined />}>返回({count}s)</Button>
+
+                </div>
+            </div>
+        </div>
+    )
+}
 export default NotFound;
